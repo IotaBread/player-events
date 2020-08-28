32a51746dee8c2bca4361737d320a27e9b6a35e4
ByMartrixX <martinlenam871@gmail.com>
2020-08-28 12:41:08 -0400

    Revert "Start to work on leave messages"
    This reverts commit 6262adda1fa307a3584a550b0c6f1baffe73bdb0.

--- src/main/java/io/github/bymartrixx/join_messages/config/JoinMessagesConfig.java
+++ src/main/java/io/github/bymartrixx/join_messages/config/JoinMessagesConfig.java
@@ -7,40 +7,18 @@ import net.minecraft.text.LiteralText;
 import net.minecraft.util.Util;
 
 public class JoinMessagesConfig {
-    private String[] joinMessages;
-    private String[] leaveMessages;
+    private String[] messages;
+
+    public String[] getMessages() {
+        return this.messages;
+    }
 
     public JoinMessagesConfig() {
-        this.joinMessages = new String[] {};
-        this.leaveMessages = new String[] {};
+        this.messages = new String[] {};
     }
 
-    public String[] getJoinMessages() {
-        return this.joinMessages;
-    }
-
-    public String[] getLeaveMessages() {
-        return this.leaveMessages;
-    }
-
-    public void runJoinMessages(MinecraftServer server, ServerPlayerEntity player) {
-        for (String message : joinMessages) {
-            if (message.charAt(0) == '/') {
-                server.getCommandManager().execute(server.getCommandSource(), Utils.messageAsString(message, player));
-            }
-            else {
-                String text = Utils.messageAsString(message, player);
-                server.sendSystemMessage(new LiteralText(text), Util.NIL_UUID);
-
-                for (ServerPlayerEntity sPlayerEntity : server.getPlayerManager().getPlayerList()) {
-                    sPlayerEntity.sendSystemMessage(Utils.messageAsText(message, player), Util.NIL_UUID);
-                }
-            }
-        }
-    }
-
-    public void runLeaveMessages(MinecraftServer server, ServerPlayerEntity player) {
-        for (String message : leaveMessages) {
+    public void runMessages(MinecraftServer server, ServerPlayerEntity player) {
+        for (String message : messages) {
             if (message.charAt(0) == '/') {
                 server.getCommandManager().execute(server.getCommandSource(), Utils.messageAsString(message, player));
             }
