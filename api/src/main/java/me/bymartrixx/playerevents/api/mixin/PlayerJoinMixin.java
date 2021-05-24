15cf809919c77bdafcc89b1d0feb4d98e5efa960
ByMartrixx <bymartrixx@gmail.com>
2021-05-24 19:13:24 -0400

    Fix crash on snapshots because of an intermediary change

--- api/src/main/java/me/bymartrixx/playerevents/api/mixin/PlayerJoinMixin.java
+++ api/src/main/java/me/bymartrixx/playerevents/api/mixin/PlayerJoinMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerManager.class)
 public class PlayerJoinMixin {
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;onSpawn()V"), method = "onPlayerConnect", cancellable = true)
+    @Inject(at = @At(value = "TAIL"), method = "onPlayerConnect", cancellable = true)
     private  void onPlayerJoin(ClientConnection connection, ServerPlayerEntity player, CallbackInfo info) {
         PlayerJoinCallback.EVENT.invoker().joinServer(player, player.getServer());
 
