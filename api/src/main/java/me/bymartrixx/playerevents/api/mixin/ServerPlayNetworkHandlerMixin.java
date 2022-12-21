1f0f30fa327e95908f6d98d4ac1a6aaa5f1eb0f2
ByMartrixx <bymartrixx@gmail.com>
2022-12-21 17:17:02 -0300

    Update to 1.19.3

--- api/src/main/java/me/bymartrixx/playerevents/api/mixin/ServerPlayNetworkHandlerMixin.java
+++ api/src/main/java/me/bymartrixx/playerevents/api/mixin/ServerPlayNetworkHandlerMixin.java
@@ -7,12 +7,15 @@ import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.unmapped.C_mofgmouu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Optional;
 
 @Mixin(ServerPlayNetworkHandler.class)
 @SuppressWarnings({"InvalidInjectorMethodSignature", "MixinAnnotationTarget", "UnresolvedMixinReference"})
@@ -32,6 +35,13 @@ public class ServerPlayNetworkHandlerMixin {
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;checkForSpam()V"),
             method = "method_44356", require = 0)
     private void onCommandExecuted(ChatCommandC2SPacket packet, CallbackInfo ci) {
+        CommandExecutionCallback.EVENT.invoker().onExecuted(packet.command(), this.player.getCommandSource());
+    }
+
+    // 1.19.3
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;checkForSpam()V"),
+            method = "m_bbegevsu", require = 0)
+    private void onCommandExecuted(ChatCommandC2SPacket packet, Optional<C_mofgmouu> optional, CallbackInfo ci) {
         CommandExecutionCallback.EVENT.invoker().onExecuted(packet.command(), this.player.getCommandSource());
     }
 
