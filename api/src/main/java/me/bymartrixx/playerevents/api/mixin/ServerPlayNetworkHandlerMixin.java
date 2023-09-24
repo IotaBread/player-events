3bde4b58eeb39c42293a26ef202b72277b9f18c1
ArnoKeesmand <arnokeesman@gmail.com>
2023-09-24 19:10:01 +0200

    update to 1.20.2

--- api/src/main/java/me/bymartrixx/playerevents/api/mixin/ServerPlayNetworkHandlerMixin.java
+++ api/src/main/java/me/bymartrixx/playerevents/api/mixin/ServerPlayNetworkHandlerMixin.java
@@ -2,7 +2,6 @@ package me.bymartrixx.playerevents.api.mixin;
 
 import me.bymartrixx.playerevents.api.event.CommandExecutionCallback;
 import me.bymartrixx.playerevents.api.event.PlayerLeaveCallback;
-import net.minecraft.network.message.MessageSignatureList;
 import net.minecraft.network.packet.c2s.play.ChatCommandC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -13,23 +12,21 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Optional;
-
 @Mixin(ServerPlayNetworkHandler.class)
-@SuppressWarnings({"MixinAnnotationTarget", "UnresolvedMixinReference"})
+//don't want to suppress those warnings, custom commands actually don't work
 public class ServerPlayNetworkHandlerMixin {
     @Shadow
     public ServerPlayerEntity player;
 
     // Just after the command is executed
     // 1.20
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;checkForSpam()V"),
-            method = "method_44356(Lnet/minecraft/class_7472;Ljava/util/Optional;)V", require = 0)
-    private void onCommandExecuted(ChatCommandC2SPacket packet, Optional<MessageSignatureList> optional, CallbackInfo ci) {
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;submit(Ljava/lang/Runnable;)Ljava/util/concurrent/CompletableFuture;", shift = At.Shift.AFTER),
+            method = "onChatCommand(Lnet/minecraft/network/packet/c2s/play/ChatCommandC2SPacket;)V")
+    private void onCommandExecuted(ChatCommandC2SPacket packet, CallbackInfo ci) {
         CommandExecutionCallback.EVENT.invoker().onExecuted(packet.command(), this.player.getCommandSource());
     }
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;onDisconnect()V"), method = "onDisconnected")
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/network/listener/AbstractServerPacketHandler;onDisconnected(Lnet/minecraft/text/Text;)V"), method = "onDisconnected")
     private void onPlayerLeave(Text reason, CallbackInfo info) {
         PlayerLeaveCallback.EVENT.invoker().leaveServer(this.player, this.player.getServer());
     }
